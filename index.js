dd492d8e08e162993cbada61ee0f0e7253504693
Karina Pliego <karinapliegovergara@gmail.com>
2023-03-31 12:56:58 -0500

    Looping lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+/*
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy Birthday to me!`);
+    debugger;
+}
+*/
+
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+*/
+
+function writeCards(names, eventName) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+        debugger;
+    }
+    return messages;
+}
+writeCards(names, eventName);
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
